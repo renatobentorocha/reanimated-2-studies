03582e703c92a88fb80d24a7b84cc3bc4acbe7e4
Renato Rocha <renatorocha@Suportes-MacBook-Pro.local>
2021-03-19 22:46:06 -0300

    Pan Gesture

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Worklets } from '../screens/Worklets';
 import { HomeScreen } from '../screens/Home';
+import PanGesture from '../screens/PanGesture';
 
 export type RootStackParamList = {
   Home: undefined;
   Worklets: undefined;
+  PanGesture: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -18,6 +20,7 @@ function Route() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Worklets" component={Worklets} />
+        <Stack.Screen name="PanGesture" component={PanGesture} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../routes';
-import { TouchableOpacity, View, Text } from 'react-native';
+
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  FlatList,
+  Dimensions,
+} from 'react-native';
 
 type WorkletsScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -12,23 +19,46 @@ type Props = {
   navigation: WorkletsScreenNavigationProp;
 };
 
+const { width } = Dimensions.get('window');
+
 export function HomeScreen({ navigation }: Props) {
+  const items = () => [
+    <TouchableOpacity
+      key={1}
+      style={{
+        width: '100%',
+        height: 55,
+        backgroundColor: '#3a3ad1',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      onPress={() => navigation.navigate('Worklets')}
+    >
+      <Text style={{ color: 'white' }}>Worklets & Shared Values</Text>
+    </TouchableOpacity>,
+    <TouchableOpacity
+      key={2}
+      style={{
+        width: '100%',
+        height: 55,
+        backgroundColor: '#3a3ad1',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      onPress={() => navigation.navigate('PanGesture')}
+    >
+      <Text style={{ color: 'white' }}>Pan Gesture</Text>
+    </TouchableOpacity>,
+  ];
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={{ width: '100%' }}>
-        <TouchableOpacity
-          style={{
-            width: '100%',
-            height: 55,
-            backgroundColor: '#3a3ad1',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onPress={() => navigation.navigate('Worklets')}
-        >
-          <Text style={{ color: 'white' }}>Worklets & Shared Values</Text>
-        </TouchableOpacity>
-      </View>
+      <FlatList
+        ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
+        contentContainerStyle={{ width }}
+        data={items()}
+        renderItem={({ item }) => item}
+      />
     </View>
   );
 }
